f30c708aabca9522bb9c55dfce329ff41d79eb7a
Yves St. Germain <yv.desousa@gmail.com>
2014-10-11 17:07:58 -0400

    views for home and repo profile

--- Gemfile
+++ Gemfile
@@ -3,17 +3,3 @@ ruby '2.1.2'
 gem "sinatra"
 gem "rake"
 gem "sinatra-activerecord"
-gem 'rack-flash3'
-gem 'json'
-gem 'require_all'
-gem "tux"
-gem "bcrypt"
-
-group :development do
-  gem "sqlite3"
-  gem "rerun"
-end
-
-group :production do
-  gem "pg"
-end
--- app.rb
+++ app.rb
@@ -1,19 +1,6 @@
 require 'sinatra'
-require 'rack-flash'
-require 'json'
-require 'require_all'
-require 'sinatra/activerecord'
-require 'bcrypt'
-require_all 'config' #database configuration
-require_all 'models' #model loads
-
-configure do
-  enable :sessions
-  use Rack::Flash
-end
 
 get '/' do
-
   erb :index
 end
 
@@ -21,37 +8,11 @@ get "/repo" do
   erb :repo
 end
 
-post '/login' do
-  puts params.inspect
-  if User.where("email = ? OR user_name = ?", params[:email], params[:email]).first.try(:authenticate, params[:password])
-    return erb :repo
-  end
-  flash[:notice] = "Incorrect Email/Username and Password combination"
-  erb :index
+
+get "/home" do
+  erb :home
 end
 
-post '/signup' do
-  puts params.inspect
-end
-
-get '/email_available' do
-  content_type :json
-  puts params.inspect
-  users = User.where(email: params[:email])
-  if users.empty?
-    return {message: "yes"}.to_json
-  else
-    return {message: "no"}.to_json
-  end
-end
-
-get '/user_name_available' do
-  content_type :json
-  puts params.inspect
-  users = User.where(user_name: params[:user_name])
-  if users.empty?
-    return {message: "yes"}.to_json
-  else
-    return {message: "no"}.to_json
-  end
+get "/repo2" do
+  erb :repo2
 end
